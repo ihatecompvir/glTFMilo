5eaab458e92bc1f6cb7b92110a3fc72f0491b23b
Nathan <nathanhurst03@gmail.com>
2025-07-09 17:05:30 -0600

    Fix argument parsing errors being ignored, causing null reference exceptions later on

--- Source/Program.cs
+++ Source/Program.cs
@@ -6,28 +6,30 @@ namespace glTFMilo.Source
     {
         static void Main(string[] args)
         {
-            var res = Parser.Default.ParseArguments<Options>(args);
+            Parser.Default.ParseArguments<Options>(args).WithParsed((options) =>
+            {
+                // Sanity check
+                if (options == null)
+                {
+                    Console.WriteLine("Unknown error from parsing arguments");
+                    return;
+                }
 
-            // if res.Value.Input ends in a milo extension, we run Program
-            // if it ends in a glb or gltf extension, we run Program
-            if (res == null)
-            {
-                Console.WriteLine("Error parsing arguments");
-                return;
-            }
-
-            if (res.Value.Input.EndsWith(".milo_xbox", StringComparison.OrdinalIgnoreCase))
-            {
-                MiloGLTFUtils.Source.MiloglTF.Program.Run(res.Value);
-            }
-            else if (res.Value.Input.EndsWith(".glb", StringComparison.OrdinalIgnoreCase) || res.Value.Input.EndsWith(".gltf", StringComparison.OrdinalIgnoreCase))
-            {
-                MiloGLTFUtils.Source.glTFMilo.Program.Run(res.Value);
-            }
-            else
-            {
-                Console.WriteLine("Unsupported file type. Please provide a .milo, .glb, or .gltf file.");
-            }
+                // if res.Value.Input ends in a milo extension, we run Program
+                // if it ends in a glb or gltf extension, we run Program
+                if (options.Input.EndsWith(".milo_xbox", StringComparison.OrdinalIgnoreCase))
+                {
+                    MiloGLTFUtils.Source.MiloglTF.Program.Run(options);
+                }
+                else if (options.Input.EndsWith(".glb", StringComparison.OrdinalIgnoreCase) || options.Input.EndsWith(".gltf", StringComparison.OrdinalIgnoreCase))
+                {
+                    MiloGLTFUtils.Source.glTFMilo.Program.Run(options);
+                }
+                else
+                {
+                    Console.WriteLine("Unsupported file type. Please provide a .milo, .glb, or .gltf file.");
+                }
+            });
         }
     }
 }
